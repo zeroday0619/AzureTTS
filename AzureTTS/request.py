2ff891123645f7562362c26ac159121379369c60
Euiseo Cha <zeroday0619_dev@outlook.com>
2022-08-10 23:07:39 +0900

    Create request.py

--- AzureTTS/request.py
+++ AzureTTS/request.py
@@ -92,7 +92,7 @@ class MicrosoftTTS:
         async with aiohttp.ClientSession(
                 headers={
                     "Content-Type": "application/ssml+xml",
-                    "X-Microsoft-OutputFormat": "riff-48khz-16bit-mono-pcm"",
+                    "X-Microsoft-OutputFormat": "riff-48khz-16bit-mono-pcm",
                     "Authorization": f"Bearer {await self.get_access_token()}"
                 }
         ) as req:
